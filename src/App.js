cb03f283e706610c0ff2854aa0ce58d93c5759a4
mogulaganirajesh1421 <mogulaganirajesh555@gmail.com>
2026-01-07 20:53:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React from "react";
-import "./App.css";
+import Team from "./components/Team"; // correct path
+import "./App.css"; // correct path: App.css is in the same folder as App.js
 
 function App() {
-  const leaveData = [
-    { id: 1, name: "Y.Mohan kumar", date: "12,13,14,16" },
-    { id: 2, name: "M.Jeevan Kumar", date: "12,13,14,16" },
-    { id: 3, name: "V. shiva subrahamanyam", date: "12,13,14,16" },
-    { id: 4, name: "M.Rajesh", date: "14,16" },
-    { id: 5, name: "Sai lakshmi", date: "14,16" },
-    { id: 6, name: "B.Gopi Prasanna", date: "16" },
-    { id: 7, name: "Thrived", date: "16" },
-  ];
-
   return (
-    <div className="container">
-      <h1>Leave Details</h1>
-
-      <table>
-        <thead>
-          <tr>
-            <th>S.No</th>
-            <th>Name</th>
-            <th>Date of Leave</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {leaveData.map((item, index) => (
-            <tr key={item.id}>
-              <td>{index + 1}</td>
-              <td>{item.name}</td>
-              <td>{item.date}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="App">
+      <Team />
     </div>
   );
 }
